c7e89061df7d7b754d6375593d9f649ec3d3b71a
Jona40 <00014620@uca.edu.sv>
2026-05-26 19:04:35 -0600

    service impl ya terminado

--- Products-Backend/entregas_labo2/Products-Backend/src/main/java/org/example/productsbackend/services/impl/SpecimenServiceImpl.java
+++ Products-Backend/entregas_labo2/Products-Backend/src/main/java/org/example/productsbackend/services/impl/SpecimenServiceImpl.java
@@ -1,18 +1,37 @@
 package org.example.productsbackend.services.impl;
 
-import com.jetbrains.exported.JBRApi;
 import lombok.RequiredArgsConstructor;
-import org.example.productsbackend.common.mapper.SpecimenMapper;
+import org.example.productsbackend.domain.dto.request.CreateSpecimenRequest;
+import org.example.productsbackend.domain.dto.request.UpdateSpecimenRequest;
+import org.example.productsbackend.domain.dto.response.PageableResponse;
+import org.example.productsbackend.domain.dto.response.SpecimenResponse;
+import org.example.productsbackend.domain.entities.Specimen;
 import org.example.productsbackend.exceptions.ResourceNotFoundException;
+import org.example.productsbackend.common.mappers.SpecimenMapper;
+import org.example.productsbackend.repositories.SpecimenRepository;
 import org.example.productsbackend.services.SpecimenService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Pageable;
+import java.util.UUID;
 
-@JBRApi.Service
+@Service
 @RequiredArgsConstructor
 public class SpecimenServiceImpl implements SpecimenService {
+
     private final SpecimenRepository specimenRepository;
     private final SpecimenMapper specimenMapper;
+
+    @Override
+    @Transactional
+    public SpecimenResponse createSpecimen(CreateSpecimenRequest request) {
+        Specimen savedSpecimen = specimenRepository.save(specimenMapper.toEntityCreate(request));
+        return specimenMapper.toDto(savedSpecimen);
+    }
 
     @Override
     public PageableResponse<SpecimenResponse> getAllSpecimens(int page, int size, String sortBy, String sortOrder) {
@@ -24,11 +43,32 @@ public class SpecimenServiceImpl implements SpecimenService {
         Page<Specimen> specimenPage = specimenRepository.findAll(pageable);
 
         if (specimenPage.isEmpty()) {
-            throw new ResourceNotFoundException("No hay specimens en Hyrule records");
+            throw new ResourceNotFoundException("No hay specimens registrados en Hyrule records");
         }
 
         return specimenMapper.toPageableResponse(specimenPage);
     }
 
+    @Override
+    public SpecimenResponse getSpecimenById(UUID id) {
+        return specimenRepository.findById(id)
+                .map(specimenMapper::toDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Specimen no encontrado en Hyrule Records"));
+    }
 
+    @Override
+    @Transactional
+    public SpecimenResponse updateSpecimen(UUID id, UpdateSpecimenRequest request) {
+        this.getSpecimenById(id);
+        Specimen toUpdate = specimenMapper.toEntityUpdate(request, id);
+        return specimenMapper.toDto(specimenRepository.save(toUpdate));
+    }
+
+    @Override
+    @Transactional
+    public SpecimenResponse deleteSpecimen(UUID id) {
+        SpecimenResponse existSpecimen = this.getSpecimenById(id);
+        specimenRepository.deleteById(id);
+        return existSpecimen;
+    }
 }
