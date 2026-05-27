d9368cda7fae0f8ca3dcfff619b80e2f389c6f59
Jona40 <00014620@uca.edu.sv>
2026-05-26 18:08:58 -0600

    creacion del global response handler

---
+++ Products-Backend/entregas_labo2/Products-Backend/src/main/java/org/example/productsbackend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package org.example.productsbackend.exceptions;
+
+import org.example.productsbackend.domain.dto.response.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDate;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleResourceNotFound(ResourceNotFoundException e) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    private ResponseEntity<ApiErrorResponse> buildErrorResponse(HttpStatus status, String message) {
+        String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().getPath();
+        return ResponseEntity.status(status).body(
+                ApiErrorResponse.builder()
+                        .message(message)
+                        .status(status.value())
+                        .time(LocalDate.now())
+                        .uri(uri)
+                        .build()
+        );
+    }
+}
